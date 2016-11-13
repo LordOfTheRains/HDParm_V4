420b3f99582437f9bae397a218ab22de52f743f4
cjhammons <cjh0030@auburn.edu>
2016-11-13 00:20:38 -0600

    fixed check_valid

--- src/kern/userprog/file.c
+++ src/kern/userprog/file.c
@@ -169,7 +169,7 @@ check_valid(int *fd) {
         return EBADF;
     }
     //check valid fileTable
-    if (ft == NULL || curthread->t_fileTable[fd]) {
+    if (curthread->t_fileTable == NULL || curthread->t_fileTable[fd]) {
         return EBADF;
     }
     return 0;
